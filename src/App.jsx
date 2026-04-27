b9c2bda22caaa9fd57f97c8c0ed0c85f7eb08c15
Areeb <muhammadareeb334@gmail.com>
2026-04-27 16:49:25 +0500

    Fixed for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 import GetQuote from "./Pages/GetQuote";
 import Layout from "./Layout/Layout";
 import Products from "./Pages/Products";
